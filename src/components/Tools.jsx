5a37851cd873aec8c68d690f8f964b2283c53afb
yeehawtoast <cpsmith96@gmail.com>
2024-11-13 13:24:45 -0700

    move imgs folder

--- src/components/Tools.jsx
+++ src/components/Tools.jsx
@@ -7,22 +7,22 @@ function Tools() {
         Tools I Work With
       </h1>
         <div className="grid grid-cols-3 sm:grid-cols-4 lg:grid-cols-6 gap-12 place-items-center">
-          <img src="imgs/csharp.svg" alt="C#" className="size-24" />
-          <img src="imgs/go.svg" alt="Go" className="size-24" />
-          <img src="imgs/js.svg" alt="Javascript" className="size-24" />
-          <img src="imgs/java.svg" alt="Java" className="size-24" />
-          <img src="imgs/python.svg" alt="Python" className="size-24" />
-          <img src="imgs/postgresql.svg" alt="postgresql" className="size-24" />
-          <img src="imgs/dotnet.svg" alt=".Net" className="size-24" />
-          <img src="imgs/dotnetcore.svg" alt=".Net Core" className="size-24" />
-          <img src="imgs/react.svg" alt="React" className="size-24" />
-          <img src="imgs/tailwind.svg" alt="Tailwind" className="size-24" />
-          <img src="imgs/ado.svg" alt="AzureDevOps" className="size-24" />
-          <img src="imgs/bash.svg" alt="Bash" className="size-24" />
-          <img src="imgs/blazor.svg" alt="Blazor" className="size-24" />
-          <img src="imgs/debian.svg" alt="Debian" className="size-24" />
-          <img src="imgs/sqldev.svg" alt="SQL Developer" className="size-24" />
-          <img src="imgs/oracle.svg" alt="Oracle" className="size-24" />
+          <img src="../../public/imgs/csharp.svg" alt="C#" className="size-24" />
+          <img src="../../public/imgs/go.svg" alt="Go" className="size-24" />
+          <img src="../../public/imgs/js.svg" alt="Javascript" className="size-24" />
+          <img src="../../public/imgs/java.svg" alt="Java" className="size-24" />
+          <img src="../../public/imgs/python.svg" alt="Python" className="size-24" />
+          <img src="../../public/imgs/postgresql.svg" alt="postgresql" className="size-24" />
+          <img src="../../public/imgs/dotnet.svg" alt=".Net" className="size-24" />
+          <img src="../../public/imgs/dotnetcore.svg" alt=".Net Core" className="size-24" />
+          <img src="../../public/imgs/react.svg" alt="React" className="size-24" />
+          <img src="../../public/imgs/tailwind.svg" alt="Tailwind" className="size-24" />
+          <img src="../../public/imgs/ado.svg" alt="AzureDevOps" className="size-24" />
+          <img src="../../public/imgs/bash.svg" alt="Bash" className="size-24" />
+          <img src="../../public/imgs/blazor.svg" alt="Blazor" className="size-24" />
+          <img src="../../public/imgs/debian.svg" alt="Debian" className="size-24" />
+          <img src="../../public/imgs/sqldev.svg" alt="SQL Developer" className="size-24" />
+          <img src="../../public/imgs/oracle.svg" alt="Oracle" className="size-24" />
         </div>
       </div>
     </>
